a97f19f8b8046812f49aa833634c8155dd4c244b
Neider Sinisterra <neidersinis@hotmail.com>
2024-06-08 17:02:43 -0500

    commit prueba

---
+++ app/loader.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { BounceLoader } from "react-spinners";
+
+import Box from "@/app/components/Box";
+
+const Loading = () => {
+    return (
+        <Box className="h-full flex items-center justify-center">
+            <BounceLoader color="#3b22c5" size={40} />
+        </Box>
+    );
+}
+
+export default Loading;
